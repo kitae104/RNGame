1cb8c513eeac48a5a71af37d276f5c72ea031098
Kitae's Github <aqua0405@gmail.com>
2024-05-17 23:47:31 +0900

    240517-2347

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { TextInput, View, Button, StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+const StartGameScreen = () => {
+    return (
+        <View>
+            <TextInput />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+    );
+};
+
+export default StartGameScreen;
