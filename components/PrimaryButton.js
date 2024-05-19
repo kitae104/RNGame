372e65ea02776bea4a9e7032b9a8a56f20c25248
Kitae's Github <aqua0405@gmail.com>
2024-05-19 22:36:07 +0900

    240519-2236

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { View, Text} from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 const PrimaryButton = ({ children }) => {
-    return (
-        <View>
-            <Text>{children}</Text>
-        </View>
-    );
+  const pressHandler = () => {
+    console.log('pressed');
+  };
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({pressed}) =>   
+        pressed     // pressed가 true일 때 스타일을 적용
+            ? [styles.buttonInnerContainer, styles.pressed] // pressed일 때 스타일
+            : styles.buttonInnerContainer   // pressed가 아닐 때 스타일
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden', // 자식 컴포넌트가 부모 컴포넌트를 벗어나는 부분을 숨김
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
-import { TextInput, View, Button, StyleSheet } from "react-native";
-import PrimaryButton from "../components/PrimaryButton";
+import { TextInput, View, Button, StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 
 const StartGameScreen = () => {
   return (
@@ -11,8 +11,14 @@ const StartGameScreen = () => {
         autoCapitalize="none"
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -21,18 +27,20 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center', // 수직 정렬
+    alignItems: 'center', // 수평 정렬
     //flex: 1,            // 가능한 공간을 모두 차지
     marginTop: 100, // 위쪽으로 100px 이동
     marginHorizontal: 24, // 좌우 여백 24px
     padding: 16, // 내부 공백 16px
-    backgroundColor: "#72063c", // 배경색
+    backgroundColor: '#2e0118', // 배경색
     borderRadius: 8, // 모서리 둥글게
 
     // Android
     elevation: 4, // 그림자(only Android)
 
     // iOS
-    shadowColor: "black", // 그림자 색
+    shadowColor: 'black', // 그림자 색
     shadowOffset: { width: 0, height: 2 }, // 그림자 위치
     shadowRadius: 6, // 그림자 반경
     shadowOpacity: 0.25, // 그림자 투명도
@@ -41,11 +49,17 @@ const styles = StyleSheet.create({
     height: 50, // 높이 50px
     width: 50, // 너비 50px
     fontSize: 32, // 글자 크기 32px
-    borderBottomColor: "#ddb52f", // 아래 테두리 색
+    borderBottomColor: '#ddb52f', // 아래 테두리 색
     borderBottomWidth: 2, // 아래 테두리 두께
-    color: "#ddb52f", // 글자 색
+    color: '#ddb52f', // 글자 색
     marginVertical: 8, // 상하 여백 8px
-    fontWeight: "bold", // 글자 굵게55555
-    textAlign: "center", // 가운데 정렬
+    fontWeight: 'bold', // 글자 굵게55555
+    textAlign: 'center', // 가운데 정렬
   },
+  buttonsContainer: {
+    flexDirection: 'row', // 수평 정렬
+  },
+  buttonContainer: {
+    flex: 1, // 가능한 공간을 모두 차지
+  }
 });
